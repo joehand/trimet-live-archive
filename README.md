4e5c0366f3c43ac2ba20d8a27db2cf99c67f4d1d
Joe Hand <joe@hand.email>
2017-07-31 13:31:48 -0700

    add some options and allow fs

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # trimet-live-archive
 
-get live trimet vehicle data into a dat
+get live trimet vehicle data into a dat or regular fs file
 
-* adds to files, `raw.json` and `vehicles.json`. 
-* `raw.json` is the raw response
-* `vehicles.json` is parsed data to ndjson geosjon values
+* data parsed to ndjson values (optionally as geojson)
 
 ## Install
 
@@ -34,16 +32,37 @@ trimet.fetch(function (err) {
 
 ```
 
+#### Using Regular Fs
+
+You can write to a regular fs file too!
+
+```js
+var trimetDat = require('trimet-live-archive')
+var appID = 'YOUR_APP_ID'
+
+var trimet = trimetDat({appID: appID, dataFile: 'data.json'})
+trimet.fetch(function (err) {
+  if (err) throw err
+  console.log('done')
+})
+```
+
 ## API
 
 ### `var trimet = trimetDat(archive, opts)`
 
-* `archive`: hyperdrive archive
+* `archive`: hyperdrive archive or fs
 * `opts.appID`: required trimet app id
+* `opts.geojson`: write data as geojson
+* `opts.dataFile`: path to write json data to
 
 #### `trimet.fetch(cb)`
 
 Fetch live vehicle data! Writes to archive files.
+
+### Debugging
+
+Use `DEBUG=trimet-live-archive` when running the process.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,42 +1,69 @@
 var assert = require('assert')
+var fs = require('fs')
 var request = require('request')
 var ndjson = require('ndjson')
 var pump = require('pump')
 var debug = require('debug')('trimet-live-archive')
 
 module.exports = function (archive, opts) {
-  assert.ok(archive, 'archive required')
+  if (!opts) {
+    opts = archive
+    archive = fs
+  }
+  assert.ok(opts.appID, 'trimet appID required')
+
   var appID = opts.appID || process.env.APP_ID
   var liveURL = `https://developer.trimet.org/ws/v2/vehicles?appID=${appID}`
+  var file = opts.dataFile || '/vehicles.json'
 
   return {
     fetch: function (cb) {
+      if (!cb) cb = function (err) { debug('[ERROR]', err) }
       debug('fetching data', liveURL)
+
       request(liveURL, {json: true}, function (err, response, body) {
         if (err || response.statusCode !== 200) return cb(err)
         var vehicleData = body.resultSet.vehicle
         debug('received data, vehicles:', vehicleData.length)
 
-        archive.writeFile('raw.json', body, function (err) {
-          if (err) return cb(err)
-          var serialize = ndjson.serialize()
-          var ws = archive.createWriteStream('vehicles.json')
-          pump(serialize, ws, cb)
+        var serialize = ndjson.serialize()
+        var ws = archive.createWriteStream(file)
+        pump(serialize, ws, cb)
 
-          vehicleData.map(function (vehicle) {
-            var coordinates = [vehicle.longitude, vehicle.latitude]
-            var geojson = {
-              'type': 'Feature',
-              'geometry': {
-                'type': 'Point',
-                'coordinates': coordinates
-              },
-              'properties': vehicle
-            }
-            serialize.write(geojson)
+        // make to stream
+        vehicleData.map(function (vehicle) {
+          // skip buses that don't have a next stop or route number
+          if (!vehicle.nextStopSeq || !vehicle.routeNumber) return
+
+          // whitelist some properties for now
+          // see details:
+          //   https://developer.trimet.org/ws_docs/vehicle_locations_ws.shtml
+          var data = {
+            id: vehicle.vehicleID,
+            time: vehicle.time,
+            longitude: vehicle.longitude,
+            latitude: vehicle.latitude,
+            type: vehicle.type,
+            routeNumber: vehicle.routeNumber,
+            sign: vehicle.signMessage,
+            delay: vehicle.delay,
+            inCongestion: vehicle.inCongestion,
+            loadPercentage: vehicle.loadPercentage,
+            direction: vehicle.direction,
+            beaing: vehicle.bearing
+          }
+
+          if (!opts.geojson) return serialize.write(data)
+          serialize.write({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [data.longitude, data.latitude]
+            },
+            properties: data
           })
-          serialize.end()
         })
+        serialize.end()
       })
     }
   }
